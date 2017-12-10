e45e9805bceacff1dd03e7be46034975e33ecfb9
Jonathan Bowen <jjbowen19@gmail.com>
2017-12-10 13:38:55 -0500

    Rewrite manual switch code to use RPi.GPIO library instead of wiringPi

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
+import RPi.GPIO as GPIO
 
-
-gpio -g mode 17 out
+GPIO.setup(17, GPIO.OUT)
 
 while True:
     switch_pos = input("Type 'on' to switch relay:\n> ")
     if switch_pos == 'on':
-        gpio -g write 17 1
+        GPIO.output(17, HIGH)
     # usleep(0.2)
         for x in range(1000):
             pass
-    gpio -g write 17 0
+    GPIO.output(17, LOW)
+
+GPIO.cleanup()
+
+
+# from wiringPi import *
+#
+# gpio -g mode 17 out
+#
+# while True:
+#     switch_pos = input("Type 'on' to switch relay:\n> ")
+#     if switch_pos == 'on':
+#         gpio -g write 17 1
+#     # usleep(0.2)
+#         for x in range(1000):
+#             pass
+#     gpio -g write 17 0
