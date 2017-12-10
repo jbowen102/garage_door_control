d16001d8d2b0aaa0c4c4f22d44f2a9e87459ca4a
Jonathan Bowen <jjbowen19@gmail.com>
2017-12-10 15:15:37 -0500

    Initially reconfig for network access.

---
+++ bin/app.py
@@ -0,0 +1,24 @@
+import web
+from garage_door_control import flip_switch
+
+# Mappings of URL to class
+urls = (
+    '/', 'Main',
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/')
+# base="layout")
+
+class Main(object):
+    def GET(self):
+        return render.switch_panel()
+
+    def POST(self):
+        form = web.input(switch1)
+        print('switch flipped')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ garage_door_control/flip_switch.py
@@ -0,0 +1,34 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.OUT)
+
+GPIO.output(17, 1)
+sleep(0.2)
+GPIO.output(17, 0)
+
+GPIO.cleanup()
+
+
+
+## Original standalone terminal-query version
+# from time import sleep
+# import RPi.GPIO as GPIO
+#
+#
+# GPIO.setmode(GPIO.BCM)
+#
+# GPIO.setup(17, GPIO.OUT)
+
+# while True:
+#     switch_pos = input("Type 'on' to switch relay:\n> ")
+#     if switch_pos == 'on':
+#         GPIO.output(17, 1)
+#     sleep(0.2)
+#
+#     GPIO.output(17, 0)
+#
+# GPIO.cleanup()
