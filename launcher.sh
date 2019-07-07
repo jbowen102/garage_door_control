cf3ed44744b2c4aae82a5c0a799ef7fc80646e55
Jonathan Bowen <jjbowen19@gmail.com>
2019-07-07 17:43:49 -0400

    Create bash launcher for startup autorun

---
+++ launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# launcher.#!/bin/sh
+# navigate to root directory, then to this directory, then execute python
+# script, then back to root
+
+cd /
+cd ~/garage_door_control
+sudo python app.py
+cd /
