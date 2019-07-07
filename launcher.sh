6faf4e3a272d26e1abe45e2090e14e4f4a712719
JB (RPI-01) <jjbowen19@gmail.com>
2019-07-07 18:07:08 -0400

    Fix file path in launcher

--- launcher.sh
+++ launcher.sh
@@ -3,10 +3,10 @@
 # navigate to root directory, then to this directory, then execute python script, then back to root
 
 cd /
-cd ~/garage_door_control
+cd home/user01/garage_door_control
 sudo python app.py
 cd /
 
 
 # added this to root crontab (using command 'sudo crontab -e':
-# @reboot sh ~/garage_door_control/launcher.sh > ~/garage_door_control/logs/cronlog 2>&1
+# @reboot sh /home/user01/garage_door_control/launcher.sh > /home/user01/garage_door_control/logs/cronlog 2>&1
