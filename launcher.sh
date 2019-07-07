b4e74a77d3b7cf83221483e81a57d023b0b2b83c
JB (RPI-01) <jjbowen19@gmail.com>
2019-07-07 17:58:46 -0400

    Update launcher script, ignore log folder

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # launcher.#!/bin/sh
-# navigate to root directory, then to this directory, then execute python
-# script, then back to root
+# navigate to root directory, then to this directory, then execute python script, then back to root
 
 cd /
 cd ~/garage_door_control
 sudo python app.py
 cd /
+
+
+# added this to root crontab (using command 'sudo crontab -e':
+# @reboot sh ~/garage_door_control/launcher.sh > ~/garage_door_control/logs/cronlog 2>&1
