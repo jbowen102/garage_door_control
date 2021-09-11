c0787e460c4c2b68f5b5a4265db3286c41749e88
Jonathan <ew15dro6k216@opayq.net>
2021-09-11 16:58:11 -0400

    Add logging function in app.py.

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
+from datetime import datetime
 import web
+
 import flip_switch
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOG_DIR = os.path.join(SCRIPT_DIR, "logs")
 
 
 # Mappings of URL to class
@@ -19,8 +26,21 @@ class Main(object):
     def POST(self):
         form = web.input()
         flip_switch.flip()
-        print('switch flipped')
+        # print('switch flipped')
+        self.log_action()
         return render.switch_panel()
+
+    def log_action(self, operation="flip"):
+        """Record activity in log file."""
+        timestamp = datetime.now().strftime("%Y-%m-%dT%H%M%S")
+        filename = "%s_%s.log" % (timestamp, operation.lower())
+
+        # Wait one second to prevent overwriting previous file if it occurred
+        # less than one second ago.
+        time.sleep(1)
+        full_path = os.path.join(LOG_DIR, filename)
+        with open(full_path, "w") as log_file:
+            log_file.write("Switch operation: %s [%s]" % (operation, timestamp))
 
 
 if __name__ == "__main__":
